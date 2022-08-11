09bd34c267dbe71528f16ca89a3e8d6bdd8addae
fumagallilaura <la_12_msn@hotmail.com>
2022-08-11 18:04:56 -0300

    fix: change typo of enemy arg on attack method in Chatacter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -3,7 +3,7 @@ import SimpleFighter from './SimpleFighter';
 interface Fighter extends SimpleFighter {
   defense: number;
   levelUp(): void;
-  special(enemy: Fighter): void;
+  special(enemy: SimpleFighter): void;
 }
 
 export default Fighter;
